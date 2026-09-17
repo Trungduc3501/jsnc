0c2fc9ac5cefafd5b3dad5b93ad935f0a2a0025d
Nguyen Duc <trungduc3501@gmail.com>
2026-09-17 15:19:05 +0700

    Lab 1 more

--- lab1.js
+++ lab1.js
@@ -1,6 +1,9 @@
+
+// B1
 console.log("Lab 1 js nang cao");
 let name ="duc";
-const age = 23;
+let age = 23;
+age = 24;
 let address ="Hà Nội";
 let isStudent="Đang còn sinh viên";
 console.log("Tên của tôi: ", name);
@@ -8,3 +11,21 @@ console.log("Tên của tôi: ", name);
 console.log("Tuổi của tôi: " ,age);
 console.log("Địa chỉ của tôi: " ,address);
 console.log("Trạng thái học tập: " ,isStudent);
+
+
+// let age= 20;
+const a =10;
+const b=5;
+
+console.log("Tổng: ",a+b );
+console.log("Hiệu: ",a-b );
+console.log("Tích: ",a*b );
+console.log("Thương: ",a/b );
+
+// b4
+function sayHello(name){
+    console.log("Xin chào ",name);
+}
+sayHello("An");
+sayHello("Duc");
+sayHello("Hoang");
